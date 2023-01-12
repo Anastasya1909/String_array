894fe09aa2383b589af2a7f95e1fb83e5f639d4b
Anastasya <ocenka.da@gmail.com>
2023-01-12 21:26:47 +0700

    variant_1

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Вариант, когда первоначальный массив задан на старте выполнения
+string[] array1 = new string[6] {"123", "hello", "and", "55555", "45", ":)"};
+string[] array2 = new string[array1.Length];
+
+Symbol(array1, array2);
+PrintArray(array2);
+
+void Symbol(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
